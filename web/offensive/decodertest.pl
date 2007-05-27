3b5ac225f921c6e76055d2693b56edc5e5be4fe1
Ray Hatfield <ray@mysocalled.com>
2007-05-27 20:11:40 +0000

    remove cruft
    git-svn-id: http://abadthing.com/tmbo/trunk@6 7ced0b6d-2a13-0410-a4a2-c2825b4e6d43

--- web/offensive/decodertest.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -w
-
-use CGI qw/escape unescape/;
-
-my $testString = 'P><a HREF="http://www.filepile.org/function.php/download/pictures/%68%6F%6D%65%6D%61%64%65%2D%73%69%6D%70%73%6F%6E%73%2D%66%69%67%75%72%69%6E%65%73%2E%6A%70%67/137446';
-
-my $decoded = unescape( $testString );
-
-print "$decoded";
