45895dc9f9b458fff3b9b9e74daac821456e382e
Richard Harman <richard@richardharman.com>
2007-10-09 05:02:02 +0000

    asshat tab
    git-svn-id: http://abadthing.com/tmbo/trunk@118 7ced0b6d-2a13-0410-a4a2-c2825b4e6d43

--- web/offensive/index.php
+++ web/offensive/index.php
@@ -231,7 +231,7 @@
 				<div class="heading">rss:</div>
 				<div class="bluebox" style="text-align:center">
 					<a href="pic_rss.php"><img src="graphics/rss_pics.gif" border="0" alt="rss: pics" width="77" height="15" style="margin-bottom:6px"></a><br/>
-					<a href="audio_rss.php"><img src="graphics/rss_mp3s.gif" border="0" alt="rss: audio" width="77" height="15" style="margin-bottom:6px"></a><br/>
+					<!--<a href="audio_rss.php"><img src="graphics/rss_mp3s.gif" border="0" alt="rss: audio" width="77" height="15" style="margin-bottom:6px"></a><br/> -->
 					<a href="zip_rss.php"><img src="graphics/rss_zips.gif" border="0" alt="rss: zips" width="77" height="15"></a>
 				</div>			
 				<div class="blackbar"></div>
