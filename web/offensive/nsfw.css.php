4cfd64fed3672bf631c5666c84e7708ae073e393
Scott Perry <github@numist.net>
2008-09-24 18:37:26 +0000

    make output from the classes a little bit more sanely typed, fix the nsfw thumbs issue.
    git-svn-id: http://abadthing.com/tmbo/trunk@310 7ced0b6d-2a13-0410-a4a2-c2825b4e6d43

--- web/offensive/nsfw.css.php
+++ web/offensive/nsfw.css.php
@@ -1,31 +1,33 @@
-<?php header( "Content-type: text/css" );
-session_start();
-?>
 <?php
-	if( array_key_exists("prefs", $_SESSION) &&
-	    is_array($_SESSION['prefs']) &&
-	    array_key_exists("hide_nsfw", $_SESSION["prefs"]) &&
-	    $_SESSION['prefs']['hide_nsfw'] == 1 ) {
-	?>		
+session_start();
+set_include_path("..");
+require("offensive/assets/header.inc");
+require("offensive/assets/classes.inc");
 
-	.nsfw {
-		background-image:url( graphics/th-nsfw.gif );
-		background-repeat:no-repeat;
-		background-position:center;
-		width:116px;
-		height:100px;
-		display:block;
-		vertical-align:center;
-	}
-	
-	.nsfw img {
-		visibility:hidden;
-	}
+header( "Content-type: text/css" );
 
-	.nsfw:hover img {
-		visibility:inherit;
-	}
+if(loggedin()) {
+	$me = new User($_SESSION["userid"]);
+	if($me->getPref("hide_nsfw") == 1) { ?>		
 
-	<?php
+		.nsfw {
+			background-image:url( graphics/th-nsfw.gif );
+			background-repeat:no-repeat;
+			background-position:center;
+			width:116px;
+			height:100px;
+			display:block;
+			vertical-align:center;
 		}
-	?>
+		
+		.nsfw img {
+			visibility:hidden;
+		}
+    	
+		.nsfw:hover img {
+			visibility:inherit;
+		}
+		<? 
+	}
+}
+?>
