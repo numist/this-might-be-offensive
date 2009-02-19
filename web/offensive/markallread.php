963d2080b33048d2f8a9a369d954b64b04ab858e
Scott Perry <github@numist.net>
2009-02-19 07:18:37 +0000

    markallread.php redirects you to the page from which you came instead of to the index.  thanks to water for reporting the unexpected behaviour.
    git-svn-id: http://abadthing.com/tmbo/trunk@384 7ced0b6d-2a13-0410-a4a2-c2825b4e6d43

--- web/offensive/markallread.php
+++ web/offensive/markallread.php
@@ -11,6 +11,8 @@
 	$sql = "UPDATE offensive_subscriptions SET commentid = NULL WHERE userid=$uid";
 	tmbo_query( $sql );
 
-	header( "Location: ./" );
-
-?>
+	if(array_key_exists("HTTP_REFERER", $_SERVER)) {
+		header( "Location: " . $_SERVER['HTTP_REFERER'] );
+	} else { ?>
+		<html><head><script type="text/javascript">history.go(-1);</script></head><body /></html>
+	<? } ?>
