83399efbc2390469a23399f2a10d679b17f18c60
Scott Perry <dev@numist.net>
2012-02-11 00:16:26 -0600

    initial import
    sanitizing queries for public db dumps

---
+++ admin/database/sanitize.sql
@@ -0,0 +1,12 @@
+DELETE FROM failed_logins;
+DELETE FROM ip_history;
+UPDATE maxxer_locations SET x = RAND(3000), y = RAND(5000), timestamp = timestamp WHERE mapversion is NULL;
+UPDATE maxxer_locations SET x = RAND(180) - 90, y = RAND(360) - 180, timestamp = timestamp WHERE mapversion = "google";
+DELETE FROM merch_buyers;
+DELETE FROM merch_items;
+DELETE FROM merch_order_item_options;
+DELETE FROM merch_order_items;
+DELETE FROM merch_orders;
+DELETE FROM referrals;
+DELETE FROM tokens;
+UPDATE users SET timestamp = timestamp, ip = "127.0.0.1", email = "spam@spammy.com", password = "ab4d8d2a5f480a137067da17100271cd176607a1";
